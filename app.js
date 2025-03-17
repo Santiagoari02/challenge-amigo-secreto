b353213036275f2df51c1bf1dad7c1e9b86731d0
Santiago Ariza <santiagoari02@outlook.com>
2025-03-17 14:56:16 -0500

    Creación función agregar amigo

--- app.js
+++ app.js
@@ -1,2 +1,20 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = []
+
+function agregarAmigo() {
+    const input = document.getElementById('amigo');
+    const nombre = input.value.trim();
+
+    if (nombre === '') {
+        alert('Por favor, ingresa un nombre válido.');
+        return;
+    }
+
+    if (amigos.includes(nombre)) {
+        alert('Este nombre ya ha sido agregado.');
+        return;
+    }
+
+    amigos.push(nombre);
+    input.value = '';
+}
