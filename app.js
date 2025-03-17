c3fcd2332a6b3b231ee00bbc577cd04a034b3b83
Santiago Ariza <santiagoari02@outlook.com>
2025-03-17 14:57:13 -0500

    Creación función actualizar amigos

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-let amigos = []
+let amigos = [];
 
 function agregarAmigo() {
     const input = document.getElementById('amigo');
@@ -17,4 +17,17 @@ function agregarAmigo() {
 
     amigos.push(nombre);
     input.value = '';
+
+    actualizarListaAmigos();
+}
+
+function actualizarListaAmigos() {
+    const listaAmigos = document.getElementById('listaAmigos');
+    listaAmigos.innerHTML = '';
+
+    amigos.forEach(amigo => {
+        const li = document.createElement('li');
+        li.textContent = amigo;
+        listaAmigos.appendChild(li);
+    });
 }
